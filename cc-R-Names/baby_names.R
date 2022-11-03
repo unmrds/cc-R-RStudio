e1a0b575e3003a6064d78173c593a2ab2787a263
Jonathan Wheeler <jonathan.wheeler01@gmail.com>
2022-11-03 11:57:37 -0600

    read and inspect 2010 data

--- cc-R-Names/baby_names.R
+++ cc-R-Names/baby_names.R
@@ -3,3 +3,14 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 
+# Read a data file into memory
+# Note: use the read_csv() function, not read.csv()
+names2010 <- read_csv("./data/2010")
+
+# Insepcting the data
+# View the data as a table in the file pane
+View(names2010)
+
+# View the data in the console
+names2010
+
